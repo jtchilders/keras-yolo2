c3071bf3c8efdb320018253b2d3527aa6fe6ddcf
Taylor Childers <taylor@jtchilders.com>
2018-11-02 15:17:56 +0000

    adding more models to test

--- backend.py
+++ backend.py
@@ -186,6 +186,304 @@ class FullYoloFeatureNCHW(BaseFeatureExtractor):
         return self.feature_extractor.get_output_shape_at(-1)[2:4]
 
 
+class FullYoloFeatureNCHW2(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_shape):
+        input_image = Input(shape=tuple(input_shape))
+        
+        # the function to implement the orgnization layer (thanks to github.com/allanzelener/YAD2K)
+        def space_to_depth_x2(x):
+            # import traceback
+            # print('------ in space_to_depth_x2 with x = ', x, '----------------------')
+            # traceback.print_stack()
+            return tf.space_to_depth(x, block_size=3, data_format="NCHW")
+
+        # Layer 1
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_1', use_bias=False, data_format="channels_first")(input_image)
+        x = BatchNormalization(axis=1,name='norm_1')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        
+        # Layer 2
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_2', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_2')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(1, 2), data_format="channels_first")(x)
+        
+        # Layer 3
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_3', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_3')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 4), data_format="channels_first")(x)
+
+        # Layer 4
+        x = Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_4', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_4')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 5
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_5', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_5')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(x)
+        
+        # Layer 6
+        x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_6')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 7
+        x = Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_7', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_7')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 8
+        x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_8', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_8')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(x)
+        
+        # Layer 9
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_9', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_9')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 10
+        x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_10', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_10')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 11
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_11', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_11')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 12
+        x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_12', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_12')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 13
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_13', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_13')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        
+        x = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(x)
+        skip_connection = x
+
+        
+
+        # Layer 14
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_14', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_14')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 15
+        x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_15', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_15')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 16
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_16', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_16')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 17
+        x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_17', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_17')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 18
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_18', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_18')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 19
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_19', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_19')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 20
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_20', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_20')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 21
+        skip_connection = Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_21', use_bias=False, data_format="channels_first")(skip_connection)
+        skip_connection = BatchNormalization(axis=1,name='norm_21')(skip_connection)
+        skip_connection = LeakyReLU(alpha=0.1)(skip_connection)
+        # skip_connection = Lambda(space_to_depth_x2)(skip_connection)
+        # print('skip_connection: ',skip_connection.shape)
+
+        x = concatenate([skip_connection, x], axis=1)
+
+        # Layer 22
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_22', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_22')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        self.feature_extractor = Model(input_image, x)
+        # Taylor: don't have model weights to load (yet)
+        # self.feature_extractor.load_weights(FULL_YOLO_BACKEND_PATH)
+
+    def normalize(self, image):
+        return image / np.amax(image)
+
+    def get_output_shape(self):
+        # return self.feature_extractor.get_output_shape_at(-1)[1:3]
+        # taylor: changed idices to match NCHW
+        return self.feature_extractor.get_output_shape_at(-1)[2:4]
+
+
+class FullYoloFeatureNCHW3(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_shape):
+        input_image = Input(shape=tuple(input_shape))
+        
+        # the function to implement the orgnization layer (thanks to github.com/allanzelener/YAD2K)
+        def space_to_depth_x2(x):
+            # import traceback
+            # print('------ in space_to_depth_x2 with x = ', x, '----------------------')
+            # traceback.print_stack()
+            return tf.space_to_depth(x, block_size=3, data_format="NCHW")
+
+        # Layer 1
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_1', use_bias=False, data_format="channels_first")(input_image)
+        x = BatchNormalization(axis=1,name='norm_1')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        
+        # Layer 2
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_2', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_2')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(1, 2), data_format="channels_first")(x)
+        
+        # Layer 3
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_3', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_3')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 4), data_format="channels_first")(x)
+
+        # Layer 4
+        x = Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_4', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_4')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(x)
+
+        # Layer 5
+        x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_5', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_5')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(x)
+        
+        # Layer 6
+        x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_6')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(1, 2), data_format="channels_first")(x)
+
+        # Layer 7
+        x = Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_7', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_7')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 8
+        x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_8', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_8')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(1, 2), data_format="channels_first")(x)
+        
+        # Layer 9
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_9', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_9')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 10
+        x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_10', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_10')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 11
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_11', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_11')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 12
+        x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_12', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_12')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 13
+        x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_13', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_13')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        
+        x = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(x)
+        skip_connection = x
+
+        
+
+        # Layer 14
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_14', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_14')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 15
+        x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_15', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_15')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 16
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_16', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_16')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 17
+        x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_17', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_17')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 18
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_18', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_18')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 19
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_19', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_19')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 20
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_20', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_20')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        # Layer 21
+        skip_connection = Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_21', use_bias=False, data_format="channels_first")(skip_connection)
+        skip_connection = BatchNormalization(axis=1,name='norm_21')(skip_connection)
+        skip_connection = LeakyReLU(alpha=0.1)(skip_connection)
+        # skip_connection = Lambda(space_to_depth_x2)(skip_connection)
+        # print('skip_connection: ',skip_connection.shape)
+
+        x = concatenate([skip_connection, x], axis=1)
+
+        # Layer 22
+        x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_22', use_bias=False, data_format="channels_first")(x)
+        x = BatchNormalization(axis=1,name='norm_22')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+
+        self.feature_extractor = Model(input_image, x)
+        # Taylor: don't have model weights to load (yet)
+        # self.feature_extractor.load_weights(FULL_YOLO_BACKEND_PATH)
+
+    def normalize(self, image):
+        return image / np.amax(image)
+
+    def get_output_shape(self):
+        # return self.feature_extractor.get_output_shape_at(-1)[1:3]
+        # taylor: changed idices to match NCHW
+        return self.feature_extractor.get_output_shape_at(-1)[2:4]
+
+
 class FullYoloFeature(BaseFeatureExtractor):
     """docstring for ClassName"""
     def __init__(self, input_shape):
